b46ec206db106b2615bb1cf1563d90146ace122d
Musaveer <musaveer05@gmail.com>
2025-06-17 00:40:11 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,42 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+// export default App;
+import React, { useEffect } from 'react';
+import { View, Button, StyleSheet, Alert } from 'react-native';
+// import { requestAndroidNotificationPermission } from './src/push_permission'; // import your file
+import CleverTap from 'clevertap-react-native';
+
+const App = () => {
+  CleverTap.setDebugLevel(2); 
+  useEffect(() => {
+    CleverTap.createNotificationChannel(
+      "CtRNS", 
+      "CleverTap React Native Testing", 
+      "CT React Native Testing", 
+      5, 
+      true
+    );
+
+  }, []);
+
+  const handlePushPress = () => {
+    Alert.alert('Button Pressed', 'You pressed the Push button!');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Button title="Push" onPress={handlePushPress} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+});
+
+export default App;
